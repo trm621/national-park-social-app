e975fc01e698ef8baeef22baff9f334a91a0dc37
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2022-03-03 15:30:09 -0500

    Added friend count logic to User Model

--- server/models/User.js
+++ server/models/User.js
@@ -62,9 +62,9 @@ userSchema.methods.isCorrectPassword = async function(password) {
   return bcrypt.compare(password, this.password);
 };
 
-// userSchema.virtual('friendCount').get(function() {
-//   return this.friends.length;
-// });
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
 
 const User = model('User', userSchema);
 
