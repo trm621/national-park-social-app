c0b3a699f6fb73995e2b9b557cfe7d52efe25611
Dawn LeBlanc <dawnleblanc11@gmail.com>
2022-03-06 20:36:10 -0500

    isolate signup feature

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,59 +1,57 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
 
-function Signup(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [addUser] = useMutation(ADD_USER);
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    const mutationResponse = await addUser({
-      variables: {
-        email: formState.email,
-        password: formState.password,
-        firstName: formState.firstName,
-        lastName: formState.lastName,
-      },
-    });
-    const token = mutationResponse.data.addUser.token;
-    Auth.login(token);
-  };
-
-  const handleChange = (event) => {
+const Signup = () => {
+  // set inital form state
+  const [userFormData, setUserFormData] = useState({ username: '',email: '', password: '' });
+  
+  
+  const [addUser, {error}] = useMutation(ADD_USER);
+  
+ // update state based on from input changes 
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
+    console.log(userFormData);
+    setUserFormData({...userFormData,[name]: value,});
   };
+
+// submit Form
+  const handleFormSubmit = async (event) => {
+    console.log('made it to mutation')
+    event.preventDefault();
+    
+    try {
+      console.log({...userFormData});
+       const { data } = await addUser({
+         variables: {...userFormData},
+       });
+       console.log(data)
+       Auth.login(data.addUser.token);
+    } catch (err) {
+      console.error(error);
+    }
+    };
+    
+
+  
 
   return (
     <div className="container my-1">
-      <Link to="/login">← Go to Login</Link>
+      {/* <Link to="/login">← Go to Login</Link> */}
 
       <h2>Signup</h2>
       <form onSubmit={handleFormSubmit}>
         <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
+          <label htmlFor="User Name">UserName:</label>
           <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
+            placeholder="Trail Runner"
+            name="username"
+            type="username"
+            id="username"
+            onChange={handleInputChange}
           />
         </div>
         <div className="flex-row space-between my-2">
@@ -63,7 +61,7 @@ function Signup(props) {
             name="email"
             type="email"
             id="email"
-            onChange={handleChange}
+            onChange={handleInputChange}
           />
         </div>
         <div className="flex-row space-between my-2">
@@ -73,7 +71,7 @@ function Signup(props) {
             name="password"
             type="password"
             id="pwd"
-            onChange={handleChange}
+            onChange={handleInputChange}
           />
         </div>
         <div className="flex-row flex-end">
