b600e74b092e542970f79394fe8d5f35f85b0ba0
Dawn LeBlanc <dawnleblanc11@gmail.com>
2022-03-08 15:04:36 -0500

    avatar start

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Redirect, useParams } from 'react-router-dom';
+import React from "react";
+import { Redirect, useParams } from "react-router-dom";
+import { Avatar, AvatarBadge } from "@chakra-ui/react";
+import { Wrap, WrapItem } from '@chakra-ui/react'
+import ThoughtForm from "../components/ThoughtForm";
+import ThoughtList from "../components/ThoughtList";
+import FriendList from "../components/FriendList";
 
-import ThoughtForm from '../components/ThoughtForm';
-import ThoughtList from '../components/ThoughtList';
-import FriendList from '../components/FriendList';
-
-import { useQuery, useMutation } from '@apollo/client';
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
-import { ADD_FRIEND } from '../utils/mutations';
-import Auth from '../utils/auth';
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import { ADD_FRIEND } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 const Profile = (props) => {
   const { username: userParam } = useParams();
@@ -51,9 +52,39 @@ const Profile = (props) => {
   return (
     <div>
       <div className="flex-row mb-3">
-        <h2 className="bg-secondary text-quinary p-3 display-inline-block" id="user-profile-header-text">
-          {userParam ? `${user.username}'s` : 'Your'} Profile
+        <h2
+          className="bg-secondary text-quinary p-3 display-inline-block"
+          id="user-profile-header-text"
+        >
+          {userParam ? `${user.username}'s` : "Your"} Profile
         </h2>
+       
+        <Wrap>
+        <WrapItem> 
+        <Avatar name='Oshigaki Kisame' src='https://bit.ly/broken-link' />
+        </WrapItem> 
+  <WrapItem>
+    <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' />
+  </WrapItem>
+  <WrapItem>
+    <Avatar name='Kola Tioluwani' src='https://bit.ly/tioluwani-kolawole' />
+  </WrapItem>
+  <WrapItem>
+    <Avatar name='Kent Dodds' src='https://bit.ly/kent-c-dodds' />
+  </WrapItem>
+  <WrapItem>
+    <Avatar name='Ryan Florence' src='https://bit.ly/ryan-florence' />
+  </WrapItem>
+  <WrapItem>
+    <Avatar name='Prosper Otemuyiwa' src='https://bit.ly/prosper-baba' />
+  </WrapItem>
+  <WrapItem>
+    <Avatar name='Christian Nwamba' src='https://bit.ly/code-beast' />
+  </WrapItem>
+  <WrapItem>
+    <Avatar name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
+  </WrapItem>
+</Wrap>
 
         {userParam && (
           <button className="btn ml-auto" onClick={handleClick}>
